c689ccf10c07e41dd5d788ffe7a9b8db9d622a44
Programmers4u <marcin@programmers4u.com>
2024-02-21 17:26:59 +0700

    wip test

--- ApiHub4aiClient.js
+++ ApiHub4aiClient.js
@@ -1,52 +1,50 @@
-import axios from "axios";
+const axios = require("axios")
+
+const requestObject = {
+    ask: "", 
+    context: "", 
+    idTask: ""
+}
 
 class ApiHub4aiClient {
     constructor(apiKey) {
-        this.apiKey = apiKey;
+        this.apiKey = apiKey
         this.apiUrl = 'https://api.programmers4u.com'
+        this.headers = {
+            'Authorization' : `Bearer ${this.apiKey}`,
+            'content-type' : 'application/json'
+        } 
     }
 
     async pingPong() {
-        const { apiKey, apiUrl } = this;
-        const headers = {
-            apikey: apiKey,
-            'content-type': 'application/json'
-        };
+        const { headers, apiUrl } = this;
+
         const response = await axios.get(`${apiUrl}/ping`, { headers });
         return response;
     }
 
     async listTasks() {
-        const { apiKey, apiUrl } = this;
-        const headers = {
-            apikey: apiKey,
-            'content-type': 'application/json'
-        };
-        const response = await axios.get(`${apiUrl}/tasks/list`, { headers });
+        const { headers, apiUrl } = this;
+
+        const response = await axios.get(`${apiUrl}/products/tasks`, { headers });
         return response;
     }
 
     async runTask(request) {
-        const { apiKey, apiUrl } = this;
-        const { ask, context, taskID } = request;
-        const headers = {
-            apikey: apiKey,
-            'content-type': 'application/json'
-        };
-        const response = await axios.post(`${apiUrl}/tasks/run`, JSON.stringify(request), { headers });
+        const { headers, apiUrl } = this;
+        // const { ask, context, idTask } = request;
+
+        const response = await axios.post(`${apiUrl}/products/tasks/query`, JSON.stringify(request), { headers });
         return response;
     }
     
     async deleteTask(request) {
-        const { apiKey, apiUrl } = this;
-        const { taskID } = request;
-        const headers = {
-            apikey: apiKey,
-            'content-type': 'application/json'
-        };
-        const response =  await axios.delete(`${apiUrl}/tasks/remove/${taskID}`, { headers })
+        const { headers, apiUrl } = this;
+
+        const { idTask } = request;
+        const response =  await axios.delete(`${apiUrl}/tasks/remove/${idTask}`, { headers })
         return response;
     }
 }
 
-export default ApiHub4aiClient
+module.exports = { ApiHub4aiClient, requestObject }
--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-import ApiHub4aiClient from 'ApiHub4aiClient.js';
-const apiKey = '';
+const { ApiHub4aiClient, requestObject } = require('./ApiHub4aiClient.js')
+
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hcmNpbkBnYXRlc21zLmV1Iiwic3ViIjoiMTI1ZTBhNzMtZDJmNy00MGVlLTljMzYtOWVjMzhmMTIzNTM1Iiwicm9sZXMiOlsidXNlciJdLCJpYXQiOjE3MDg1MDcxNjQsImV4cCI6MTcwODgwNzE2NH0.iLWRwlzZ_Nk4Tuq3VVL3xfxkHT2_pAFY-4-P_Z0Xmzc'
+
 const api = new ApiHub4aiClient(apiKey);
-api.listTasks().then((listOfTasks) => {
-    listOfTasks.forEach((task) => {
-        console.log(task);
-    })
+
+api.pingPong().then((result) => console.debug(result?.data))
+
+// api.listTasks().then((response) => response?.data).then((listOfTasks) => {
+//     listOfTasks.forEach((task) => {
+//         console.log(task)
+//     })
+// })
+
+requestObject.idTask = "fa6bd988-b0dc-495d-bf41-c66b0ac1362f"
+requestObject.context = " "
+requestObject.ask = "Disney Store is the ultimate Disney shopping destination for kids and adults. Shop from a wide assortment of products including clothes, toys, collectibles, decor and more! We also offer a variety of in-store events that are perfect for all ages. From costumed Character appearances to interactive storytelling sessions with our Disney Storytellers, there's something here for everyone."
+api.runTask(requestObject).then((response) => {
+    console.log(response?.data?.text)
 });
